aeabcd52f15d71aca930685376b23f71f36362ec
Alex Hermida <alexhermida@gmail.com>
2018-01-02 23:15:21 +0100

    Add env sample & refactor settings

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
 import os
 
+# Telegram configuration settings
+
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
-TELEGRAM_ADMINS_LIST = os.getenv('TELEGRAM_ADMINS_LIST')
+TELEGRAM_ADMINS_LIST = list(
+    map(int, os.getenv('TELEGRAM_ADMINS_LIST').split(',')))

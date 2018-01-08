0a76887438d3f9c2132b97fbbf367f8f19d86c78
Alex Hermida <alexhermida@gmail.com>
2018-01-08 14:06:59 +0100

    Add first test

--- tests.py
+++ tests.py
@@ -14,9 +14,12 @@ def updater():
     if up.running:
         up.stop()
 
+
 @patch('settings.TELEGRAM_ADMINS_LIST', [1])
 def test_reply_message(updater):
     response = reply_message('start')
 
+    update = Mock(effective_user=Mock(id=1), message=Mock())
+    next_step = response(updater.bot, update=update)
 
-    assert response(updater.bot, update=Mock(effective_user=Mock(id=1))) == MESSAGES['start']
+    assert not next_step
